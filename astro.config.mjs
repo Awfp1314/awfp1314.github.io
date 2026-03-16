4677d33e5122af5303e5d49301374a3587b2f1d3
Awfp1314 <2910385881@qq.com>
2026-03-16 21:09:57 +0800

    去掉base配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import expressiveCode from 'astro-expressive-code';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://awfp1314.github.io/',
-  base: '/HUTAO667.github.io',
   integrations: [tailwind({
     applyBaseStyles: false,
   }), expressiveCode({
